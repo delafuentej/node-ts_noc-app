0ad8870d7af004fddc606a7c64503b2cf1fde192
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-10-28 11:31:14 +0100

    crom npm-package installed && CronService

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,7 +1,18 @@
+import { CronService } from "./cron/cron-service";
 
 
 export class ServerApp {
    public static run(){
-    console.log('Server is running...')
+    console.log('Server is running...');
+
+   CronService.createJob(
+      '*/5 * * * * *',
+      ()=>{
+         const date = new Date();
+         console.log('Cron job executed every 5 seconds', date);
+      }
+   );
+
+ 
    }
 }
