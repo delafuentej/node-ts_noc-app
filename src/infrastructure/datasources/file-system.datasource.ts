dc4b723830cf20d9647179783597fbd3c2081c3b
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-10-28 16:13:57 +0100

    filesystem- saveLog implemented

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -1,15 +1,51 @@
 import { LogDatasource } from "../../domain/datasources/log.datasource";
 import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+import { writeFileSync, mkdirSync, existsSync, appendFileSync} from "fs";
 
 export class FileSystemDatasource implements LogDatasource{
 
     private readonly logPath = 'logs/';
-    private readonly lowLogsPath = 'logs/logs-low.log';
+    private readonly allLogsPath = 'logs/logs-all.log';
     private readonly mediumLogsPath = 'logs/logs-medium.log';
     private readonly highLogsPath = 'logs/logs-high.log';
 
-    saveLog(log: LogEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    constructor(){
+        this.createLogsFiles();
+    }
+
+    private createLogsFiles = () => {
+            if(!existsSync(this.logPath)){
+                mkdirSync(this.logPath);
+            };
+          [
+            this.allLogsPath,
+            this.mediumLogsPath,
+            this.highLogsPath,
+          ].forEach( path => {
+            if(existsSync(path)) return;
+            writeFileSync(path,'');
+          })
+    }
+
+    async saveLog(log: LogEntity): Promise<void> {
+
+        const logAsJson =  `${JSON.stringify(log)}\n`;
+
+       appendFileSync(this.allLogsPath,logAsJson);
+
+       switch (log.level) {
+
+        case LogSeverityLevel.medium:
+            appendFileSync(this.mediumLogsPath, logAsJson);
+            break;
+        case LogSeverityLevel.high:
+            appendFileSync(this.highLogsPath, logAsJson);
+            break;
+        case LogSeverityLevel.low:
+        default:
+            return;
+    }
+
     }
     getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
         throw new Error("Method not implemented.");
