a697c1623517be620c69cbdcf9c2b866d6304cea
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-10-31 13:09:05 +0100

    create/read  a collection + db records in mongo db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 
 import { envs } from "./config/plugins/envs.plugin";
-import { MongoDB } from "./data/mongo";
+import { LogModel, MongoDB } from "./data/mongo";
 import { ServerApp } from "./presentation/server-app";
 
 
@@ -17,7 +17,20 @@ import { ServerApp } from "./presentation/server-app";
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
    })
+   // create a db record in mongo db
+   // create a collection => a table in an relational db
+   // create a document => a record('row') in an relational db
+  //  const newLog = await LogModel.create({
+  //   message: 'Test message from Mongo',
+  //   level: 'low',
+  //   origin: 'app.ts'
+  //  });
 
+
+  //  await newLog.save();
+  //  console.log(newLog)
+  // const logs = await LogModel.find();
+  // console.log(logs[4].level);
    
     ServerApp.run()
   
--- src/data/mongo/index.ts
+++ src/data/mongo/index.ts
@@ -1,1 +1,2 @@
 export * from './getting-started';
+export * from './models/log.model';
