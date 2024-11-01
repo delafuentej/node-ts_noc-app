2d357649d28c706d2a289dc3e320c3b5d68c7773
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-11-01 15:54:33 +0100

    testing abstract class LogDatasource

--- src/domain/datasources/log.datasource.ts
+++ src/domain/datasources/log.datasource.ts
@@ -5,6 +5,6 @@ export abstract class LogDatasource {
     //abstract => prevent creating a instance of the class
     // to enforce the behaviour of this class over other classes
     //methods(business rules for  datasource):
-    abstract saveLog( log: LogEntity): Promise<void>
-    abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>
+     abstract saveLog( log: LogEntity): Promise<void>
+     abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>
 } 
